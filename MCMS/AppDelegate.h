e3ba5f5751354c19ab85724f80e7cb1846bd5dc8
tmroz <tom.mroz@d214.org>
2014-05-20 13:16:01 -0500

    Initial Commit

---
+++ MCMS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MCMS
+//
+//  Created by Thomas M. Mroz on 5/20/14.
+//  Copyright (c) 2014 BGHS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
